58f248b6f2dcd8aa12aa79d6979d1c75da2a1130
Uchemic <egegeuche25@gmail.com>
2022-05-14 09:52:17 +0100

    firts commit

---
+++ android/app/src/main/kotlin/com/digitalartnetwork/digitalart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digitalartnetwork.digitalart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/res/color_resource.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorResource {
+  Color primary = const Color(0xff1b1f28);
+
+  Color selectedTextColor = const Color(0xffb02a84);
+
+  Color unSelectedTextColor = const Color(0xff2233ee);
+}
